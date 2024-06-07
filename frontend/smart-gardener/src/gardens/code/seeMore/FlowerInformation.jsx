e57b5b2f854538b62251d7665b2cb14e8f5d56a6
leo habets <leo.habets@epita.fr>
2024-06-07 09:30:05 +0200

    last minute change

--- frontend/smart-gardener/src/gardens/code/seeMore/FlowerInformation.jsx
+++ frontend/smart-gardener/src/gardens/code/seeMore/FlowerInformation.jsx
@@ -1,175 +1,169 @@
-import React, {useEffect, useState} from "react";
-import {useCookies} from "react-cookie";
-import {Container, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { Container, Row } from "react-bootstrap";
 import axios from "axios";
-import {ADDR} from "../../../App";
+import { ADDR } from "../../../App";
 import "../../styles/seeDetails.css";
-import {
-    BarChart,
-    Bar,
-    XAxis,
-    YAxis,
-    CartesianGrid,
-    Tooltip,
-} from "recharts";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
 
 function FlowerInformation() {
-    const [flower, setFlower] = useState([]);
-    const [log, setLog] = useState({});
+  const [flower, setFlower] = useState([]);
+  const [log, setLog] = useState({});
 
-    const [cookie] = useCookies(["user"]);
-    const [visibleDetails, setVisibleDetails] = useState([]);
-    const [temperatureData, setTemperatureData] = useState([])
-    const [soilData, setSoilData] = useState([])
+  const [cookie] = useCookies(["user"]);
+  const [visibleDetails, setVisibleDetails] = useState([]);
+  const [temperatureData, setTemperatureData] = useState([]);
+  const [soilData, setSoilData] = useState([]);
 
-    const [showPopup, setShowPopup] = useState(false);
-    const [selectedProduct, setSelectedProduct] = useState(null);
+  const [showPopup, setShowPopup] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
-    useEffect(() => {
-        const formData = {
-            id: cookie.user.id,
-            pwd: cookie.user.pwd,
-            id_garden: cookie.id_Garden,
-        };
-        axios.post(ADDR + "garden/get_data_garden", formData).then((response) => {
-            const data = response.data.message;
-            const flower = data.flower_id;
-            setFlower(flower);
-            const visibility = []
-            for (let i = 0; i < flower.length; i++) {
-                visibility.push(false)
-            }
-            setVisibleDetails(visibility)
-            setLog(data.log.log);
-            console.log("Log = ",data.log.log.length)
-            console.log("flower : ", flower);
-            console.log("response.data.message : ", response.data.message);
-
-
-            // Transform temperatures and soils arrays for recharts
-            const tD = []
-            const sD = []
-
-            for (let i = 0; i < data.log.log.length; i++) {
-                const fl = data.log.log[i]
-                while (fl.temperature.length<10)
-                {
-                    fl.temperature.push(0)
-                }
-                tD.push(fl.temperature.map((value, index) => ({
-                    index,
-                    value,
-                })))
-                while (fl.soil.length<10)
-                {
-                    fl.soil.push(0)
-                }
-                sD.push(fl.soil.map((value, index) => ({index, value})));
-            }
-            setTemperatureData(tD)
-            setSoilData(sD)
-
-        });
-    }, []);
-
-    const handleSeeInfos = (index) => {
-        const vis = [...visibleDetails]
-        vis[index] = !vis[index]
-        setVisibleDetails(vis);
+  useEffect(() => {
+    const formData = {
+      id: cookie.user.id,
+      pwd: cookie.user.pwd,
+      id_garden: cookie.id_Garden,
     };
+    axios.post(ADDR + "garden/get_data_garden", formData).then((response) => {
+      const data = response.data.message;
+      const flower = data.flower_id;
+      setFlower(flower);
+      const visibility = [];
+      for (let i = 0; i < flower.length; i++) {
+        visibility.push(false);
+      }
+      setVisibleDetails(visibility);
+      setLog(data.log.log);
+      console.log("Log = ", data.log.log.length);
+      console.log("flower : ", flower);
+      console.log("response.data.message : ", response.data.message);
 
-    const handleDelete = (index) => {
-        const formData = {
-            id: cookie.user.id,
-            pwd: cookie.user.pwd,
-            id_garden: cookie.id_Garden,
-            plant_id : log[index].id
-        };
-        axios.post(ADDR + "flower/delete_flower", formData).then((response) => {})
-        window.location.href = '/seeDetails';
+      // Transform temperatures and soils arrays for recharts
+      const tD = [];
+      const sD = [];
+
+      for (let i = 0; i < data.log.log.length; i++) {
+        const fl = data.log.log[i];
+        while (fl.temperature.length < 10) {
+          fl.temperature.push(0);
+        }
+        tD.push(
+          fl.temperature.map((value, index) => ({
+            index,
+            value,
+          })),
+        );
+        while (fl.soil.length < 10) {
+          fl.soil.push(0);
+        }
+        sD.push(fl.soil.map((value, index) => ({ index, value })));
+      }
+      setTemperatureData(tD);
+      setSoilData(sD);
+    });
+  }, []);
+
+  const handleSeeInfos = (index) => {
+    const vis = [...visibleDetails];
+    vis[index] = !vis[index];
+    setVisibleDetails(vis);
+  };
+
+  const handleDelete = (index) => {
+    const formData = {
+      id: cookie.user.id,
+      pwd: cookie.user.pwd,
+      id_garden: cookie.id_Garden,
+      plant_id: log[index].id,
     };
+    axios.post(ADDR + "flower/delete_flower", formData).then((response) => {});
+    window.location.href = "/seeDetails";
+  };
 
-    const handleAddFlower = () => {
-        //Cookies ?
-        window.location.href = '/addFlowers'
-    };
+  const handleAddFlower = () => {
+    //Cookies ?
+    window.location.href = "/addFlowers";
+  };
 
+  const graphWidth = 200;
+  const graphHeight = 150;
 
-    const graphWidth = 200;
-    const graphHeight = 150;
-
-    return (
-        <>
-            <Container>
-                <Row>
-                    <div key={"title_hours"} className="header">
-                        <h1>Flowers</h1>
+  return (
+    <>
+      <Container>
+        <Row>
+          <div key={"title_hours"} className="header">
+            <h1>Flowers</h1>
+          </div>
+          {flower &&
+            flower.map((mem, index) => {
+              return (
+                <div
+                  className="gardens-container"
+                  style={{ paddingTop: 5 }}
+                  key={mem.name + "1223"}
+                >
+                  <div className="garden-card" key={mem.name}>
+                    <div className="title-container">
+                      <h2>{mem.name}</h2>
+                      <h2>{mem.id}</h2>
                     </div>
-                    {flower &&
-                        flower.map((mem, index) => {
-                            return (
-                                <div className="gardens-container" style={{paddingTop: 5}} key={mem.name + "1223"}>
-                                    <div className="garden-card" key={mem.name}>
-                                        <div className="title-container">
-                                            <h2>{mem.name}</h2>
-                                        </div>
 
-                                        <button
-                                            onClick={() =>
-                                                handleSeeInfos(index)
-                                            }
-                                            className="see-details-button"
-                                        >
-                                            {visibleDetails ? "Hide Details" : "See Details"}
-                                        </button>
-                                        <button
-                                            onClick={() => handleDelete(index)}
-                                            className="delete-button"
-                                        >
-                                            Delete
-                                        </button>
+                    <button
+                      onClick={() => handleSeeInfos(index)}
+                      className="see-details-button"
+                    >
+                      {visibleDetails ? "Hide Details" : "See Details"}
+                    </button>
+                    <button
+                      onClick={() => handleDelete(index)}
+                      className="delete-button"
+                    >
+                      Delete
+                    </button>
 
-                                        {visibleDetails[index] && (
-                                            <div className="flower-graphs">
-                                                <h5>temperature</h5>
-                                                <BarChart
-                                                    style={{transform: "translateX(-40px)"}}
-                                                    width={graphWidth}
-                                                    height={graphHeight}
-                                                    data={temperatureData[index]}
-                                                >
-                                                    <CartesianGrid strokeDasharray="3 3"/>
-                                                    <XAxis dataKey="index"/>
-                                                    <YAxis/>
-                                                    <Tooltip/>
-                                                    <Bar dataKey="value" fill="red"/>
-                                                </BarChart>
+                    {visibleDetails[index] && (
+                      <div className="flower-graphs">
+                        <h5>temperature</h5>
+                        <BarChart
+                          style={{ transform: "translateX(-40px)" }}
+                          width={graphWidth}
+                          height={graphHeight}
+                          data={temperatureData[index]}
+                        >
+                          <CartesianGrid strokeDasharray="3 3" />
+                          <XAxis dataKey="index" />
+                          <YAxis />
+                          <Tooltip />
+                          <Bar dataKey="value" fill="red" />
+                        </BarChart>
 
-                                                <h5>moisture</h5>
-                                                <BarChart
-                                                    style={{transform: "translateX(-40px)"}}
-                                                    width={graphWidth}
-                                                    height={graphHeight}
-                                                    data={soilData[index]}
-                                                >
-                                                    <CartesianGrid strokeDasharray="3 3"/>
-                                                    <XAxis dataKey="index"/>
-                                                    <YAxis/>
-                                                    <Tooltip/>
-                                                    <Bar dataKey="value" fill="blue"/>
-                                                </BarChart>
-                                            </div>
-                                        )}
-                                    </div>
-                                </div>
-                                
-                            );
-                        })}
-                </Row>
-                <button onClick={() => handleAddFlower()} className="add-button">Add</button>
-            </Container>
-        </>
-    );
+                        <h5>moisture</h5>
+                        <BarChart
+                          style={{ transform: "translateX(-40px)" }}
+                          width={graphWidth}
+                          height={graphHeight}
+                          data={soilData[index]}
+                        >
+                          <CartesianGrid strokeDasharray="3 3" />
+                          <XAxis dataKey="index" />
+                          <YAxis />
+                          <Tooltip />
+                          <Bar dataKey="value" fill="blue" />
+                        </BarChart>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+        </Row>
+        <button onClick={() => handleAddFlower()} className="add-button">
+          Add
+        </button>
+      </Container>
+    </>
+  );
 }
 
 export default FlowerInformation;
--- frontend/smart-gardener/src/gardens/code/seeMore/UserInformation.jsx
+++ frontend/smart-gardener/src/gardens/code/seeMore/UserInformation.jsx
@@ -1,67 +1,80 @@
-import React, {useEffect, useState} from "react";
-import {useCookies} from "react-cookie";
-import {Container, Row} from "react-bootstrap";
-import {ADDR} from "../../../App";
+import React, { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
+import { Container, Row } from "react-bootstrap";
+import { ADDR } from "../../../App";
 import "../../styles/seeDetails.css";
 
 function UserInformation() {
-    const [member, setMember] = useState([]);
-    const [cookie,] = useCookies(["user"]);
+  const [member, setMember] = useState([]);
+  const [cookie] = useCookies(["user"]);
+  const [token, setToken] = useState("");
 
-
-    useEffect(() => {
-        const formData = {
-            id: cookie.user.id,
-            pwd: cookie.user.pwd,
-            id_garden: cookie.id_Garden
-        };
-        fetch(ADDR + 'garden/get_data_garden', {
-            method: "POST",
-            headers: {"Content-type": "application/json"},
-            body: JSON.stringify(formData)
-        }).then(r => {
-            r.json().then((d) => {
-                const data = d.message
-                setMember(data.member);
-            })
-        })
-    }, [cookie.id_Garden, cookie.user.id, cookie.user.pwd]);
-    const handleAddUser = () => {
-        //Cookies ?
-        window.location.href = '/addUser'
+  useEffect(() => {
+    const formData = {
+      id: cookie.user.id,
+      pwd: cookie.user.pwd,
+      id_garden: cookie.id_Garden,
     };
+    fetch(ADDR + "garden/get_data_garden", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(formData),
+    }).then((r) => {
+      r.json().then((d) => {
+        const data = d.message;
+        setMember(data.member);
+        setToken(data.token);
+      });
+    });
+  }, [cookie.id_Garden, cookie.user.id, cookie.user.pwd]);
+  const handleAddUser = () => {
+    //Cookies ?
+    window.location.href = "/addUser";
+  };
 
-    return (
-        <>
-            <Container>
-                <Row>
-                    <div key={"title_hours"} className="header">
-                        <h1>All Members</h1>
-                        <h5>--</h5>
-                    </div>
-                    {member.map((mem) => {
-                        return (
-                            <div
-                                className="gardens-container"
-                                style={{paddingTop: 5}}
-                                key={mem.last_name}
-                            >
-                                <div key={mem.last_name + "Card"}>
-                                    <div>
-                                        <h5>Address : {mem.last_name}</h5>
-                                        <h5>Name : {mem.first_name}</h5>
-                                        <h5 >Id : {mem.id}</h5>
-                                        <h5>--</h5>
-                                    </div>
-                                </div>
-                            </div>)
-                    })}
-                </Row>
-                <button onClick={() => handleAddUser()} className="add-button">Add</button>
-            </Container>
-        </>
-    )
-        ;
+  return (
+    <>
+      <Container>
+        <div className="header">
+          <h1>Garden info</h1>
+
+          <h5>--</h5>
+          <h5>garden token : {token}</h5>
+          <h5>garden id : {cookie.id_Garden}</h5>
+
+          <h5>--</h5>
+        </div>
+
+        <Row>
+          <div key={"title_hours"} className="header">
+            <h1>All Members</h1>
+            <h5>--</h5>
+          </div>
+          {member.map((mem) => {
+            return (
+              <div
+                className="gardens-container"
+                style={{ paddingTop: 5 }}
+                key={mem.last_name}
+              >
+                <div key={mem.last_name + "Card"}>
+                  <div>
+                    <h5>Address : {mem.last_name}</h5>
+                    <h5>Name : {mem.first_name}</h5>
+                    <h5>Id : {mem.id}</h5>
+                    <h5>--</h5>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </Row>
+        <button onClick={() => handleAddUser()} className="add-button">
+          Add
+        </button>
+      </Container>
+    </>
+  );
 }
 
 export default UserInformation;
